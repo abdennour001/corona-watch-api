bf832aa4e3fe7ac2ce18cbd6b805f23ad87b99a7
abdennour001 <amokranabdennour@gmail.com>
2020-04-19 10:18:12 +0100

    fix settings.oy CORS

--- src/CoronaWatchAPI/settings.py
+++ src/CoronaWatchAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%k-^!r-2pfp)byx-so$*dw3w%0v7s#7mkcp2exgggf(u7+du*8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
